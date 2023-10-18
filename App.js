6470f0ff1551232987ce8e2aa28bff2ef440c31d
ThanooshDarsi <94456829+ThanooshDarsi@users.noreply.github.com>
2023-10-18 21:49:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import WordCounter from './WordCounter';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* Other content */}
+        <WordCounter />
+      </header>
+    </div>
+  );
+}
+
+export default App;
